72c5bd203d9fbf66011dd0aad571c80c2742c42e
Fabian Chinchilla Mayorga <fb.argenis@gmail.com>
2018-03-07 10:22:07 -0600

    -- clase 55: Recibiendo parametros por URL ActiveRoute

--- src/app/components/heroe/heroe.component.ts
+++ src/app/components/heroe/heroe.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HeroesService } from '../../services/heroes.services';
 
 @Component({
-  selector: 'app-heroe',
-  templateUrl: './heroe.component.html',
-  styles: []
+	selector: 'app-heroe',
+	templateUrl: './heroe.component.html',
+	styles: []
 })
 export class HeroeComponent implements OnInit {
+	heroe: any = {};
 
-  constructor() { }
+	constructor(private activatedRoute: ActivatedRoute,
+		private _heroesService: HeroesService
+	) {
+		this.activatedRoute.params.subscribe(params => {
+			// console.log(params['id']);
+			this.heroe = this._heroesService.getHeroe(params['id'])
+		});
+	}
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+	}
 
 }
