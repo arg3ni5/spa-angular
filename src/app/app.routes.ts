beb0cbaea6b5866b2988d8fc54aef54f9a435c47
Fabian Chinchilla <fb.argenis@gmail.com>
2018-05-25 06:12:49 +0000

    -- clase 60-61: tarea

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
+import { BuscadorComponent } from './components/buscador/buscador.component';
 import { HeroeComponent } from './components/heroe/heroe.component';
 
 const APP_ROUTES: Routes = [
@@ -10,6 +11,7 @@ const APP_ROUTES: Routes = [
 	{ path: 'about', component: AboutComponent },
 	{ path: 'heroes', component: HeroesComponent },
 	{ path: 'heroe/:id', component: HeroeComponent },
+	{ path: 'buscador/:termino', component: BuscadorComponent },
 	{ path: '**', pathMatch:'full', redirectTo:'home' },
 ];
 
